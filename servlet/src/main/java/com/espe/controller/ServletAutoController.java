a17253a0d4e5bcd4da798748072a234f6780c3be
Labs <myarrobo@espe.edu.ec>
2023-08-09 08:48:18 -0500

    Add

--- servlet/src/main/java/com/espe/controller/ServletAutoController.java
+++ servlet/src/main/java/com/espe/controller/ServletAutoController.java
@@ -1,7 +1,14 @@
 package com.espe.controller;
 
 import java.io.*;
+import java.time.Year;
+import java.util.List;
 
+import com.espe.dao.IAutoDAO;
+import com.espe.idao.AutoDaoImpl;
+import com.espe.model.Auto;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -13,14 +20,42 @@ public class ServletAutoController extends HttpServlet {
         message = "Hello World from Servlet!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        /**Auto auto1 = new Auto();
+        auto1.setPlaca("5562222");
+        auto1.setMarca("mazda");
+        auto1.setModelo("grand sd");
+        auto1.setAnio(Year.of(2020));
+        auto1.setCilindraje("1600 c");
+        auto1.setCosumo("20 km por galon");
+
+        Auto auto2 = new Auto();
+        auto2.setPlaca("55111");
+        auto2.setMarca("Chevrolet");
+        auto2.setModelo("grand sd");
+        auto2.setAnio(Year.of(2020));
+        auto2.setCilindraje("1600 c");
+        auto2.setCosumo("20 km por galon");
+
+        Auto auto3 = new Auto();
+        auto3.setPlaca("55888888");
+        auto3.setMarca("mercedes");
+        auto3.setModelo("grand sd");
+        auto3.setAnio(Year.of(2020));
+        auto3.setCilindraje("1600 c");
+        auto3.setCosumo("20 km por galon");
+
+        IAutoDAO autoDAO = new AutoDaoImpl();
+        autoDAO.add(auto1);
+        autoDAO.add(auto2);
+        autoDAO.add(auto3);
+
+        List<Auto> listaAutos=autoDAO.get();
+        request.setAttribute("autos", listaAutos);
+
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);**/
     }
 
     public void destroy() {
--- servlet/src/main/webapp/index.jsp
+++ servlet/src/main/webapp/index.jsp
@@ -1,12 +1,27 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>Administrar Autos</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
+<h1><%= "Listar Autos" %></h1>
+<a href="autoController?opcion=registro">Crear Auto</a>
+<table border="1">
+    <tr>
+        <th>Marca</th>
+        <th>Placa</th>
+        <th>Modelo</th>
+    </tr>
+    <c:forEach var="auto" items="${autos}">
+        <tr>
+            <td> <c:out value="${auto.marca}"></c:out> </td>
+            <td> <c:out value="${auto.placa}"></c:out> </td>
+            <td> <c:out value="${auto.modelo}"></c:out> </td>
+        </tr>
+    </c:forEach>
+</table>
 <br/>
 <a href="autoController">Hello Servlet</a>
 </body>
